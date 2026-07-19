386f45f2d4ea4ab072aa1ac0a43e87d2ea87c2ee
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2026-07-19 19:41:53 +0300

    feat(Login and Signup)

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,11 +1,17 @@
 import { Router } from 'express'
-import {  signup } from './auth.service.js';
+import {  login, signup } from './auth.service.js';
+import {successResponse} from './../../common/utils/response/index.js'
 const router = Router(); 
 router.post("/signup", async (req, res, next) => {
     const result = await signup(req.body)
-    return res.status(201).json({ message: "Done signup", result })
+    return successResponse({res,status:201,message:"User Created Successfully",data:result})
 })
 
+
+router.post("/login", async (req, res, next) => {
+    const result = await login(req.body)
+    return successResponse({res,status:200,message:"User Logged Successfully",data:result})
+})
 
 
 export default router
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,3 +1,22 @@
+import { UserModel } from "../../DB/model/user.model.js";
+
 export const signup = async (inputs) => {
-    return result
+    const {username,email,password,phone} = inputs;
+    const userExists = await UserModel.findOne({email});
+    if(userExists){
+        return ConflictException({message:"This email already exists"});
+    }
+    const [user] =await UserModel.create([{username,email,password,phone}]);
+    return user;
 }
+
+
+export const login = async (inputs) => {
+    const {email,password} = inputs;
+    const user = await UserModel.findOne({email});
+    if(!user){
+        return NotFoundException({message:"Invalid Login Credentials"});
+    }
+    return user;
+}
+
