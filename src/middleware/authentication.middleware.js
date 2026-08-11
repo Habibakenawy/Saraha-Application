a0105855c5f858b972b6a02ccdac16f0c005a8b3
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2026-08-11 21:28:51 +0300

    Update authentication.middleware.js

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -1,11 +1,28 @@
 import { TokenTypeEnum } from "../common/enum/index.js";
-import { decodeToken } from "../common/utils/index.js";
+import { BadRequestException, decodeToken } from "../common/utils/index.js";
+import { login } from "../modules/auth/auth.service.js";
 
 
 
 export const authentiction = (tokenType = TokenTypeEnum.Access) => {
 return async (req,res,next) => {
-   req.user = await decodeToken({token:req.headers.authorization,tokenType})
-    next();
+    const {authorization} = req.headers;
+    console.log(authorization);
+    //bn7ot Bearer abl el token 3lshan nfr2 mabeen el protocols
+    const [schema,credentials] = authorization.split(" ");
+    switch(schema){
+        case "Basic":
+            const [email,password] = Buffer.from(credentials,'base64')?.toString()?.split(" ") || [];
+            console.log(email,password);
+            await login({email,password},`${req.protocol}://${req.host}`);
+            break;
+        case "Bearer":
+            req.user = await decodeToken({token:credentials,tokenType})
+            break;
+        default:
+            throw BadRequestException({message:"Missing authentication schema"})
+    }
+                 next();
+ 
 }
 }
